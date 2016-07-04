41681d09b98ff9d945a03d1ee0d8cf9047fc5bd2
rashid <rash.ik@yandex.ru>
2016-07-04 16:02:40 +0300

    add text upload and representation

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ from time import sleep
 # url to our upload service, that is @app.route('/upload', ...)
 url = "http://127.0.0.1:5000/"
 
-''' Testing part '''
+''' Testing part BEGINs'''
 # from glob import glob
 #zhopy ebal
 # global_path = 'D:/BOARD_PROJECT/test_images/'
@@ -22,15 +22,16 @@ url = "http://127.0.0.1:5000/"
 ''' Testing part ENDs'''
 
 img_to_sent = 'D:/BOARD_PROJECT/test_images/13.jpg' # write your own path + filename
+text_to_sent = 'D:/BOARD_PROJECT/test_images/1.txt' # write your own path + filename
 
 while True:
     # opens img_to_sent file
-    with open(img_to_sent, 'rb') as f :
+    with open(img_to_sent, 'rb') as img, open(text_to_sent, 'rb') as txt:
         # constructs a dict from data (img_to_sent)
-        file = {'file': f}
+        files = {'pic': img, 'text': txt}
         # sends the image to the server via post request
 
-        r = requests.post(url, files=file)
+        r = requests.post(url, files=files)
         
     # waits 10 sec. before sending again.
     sleep(10) # One SHOULD have a condition when it's to send POST request but now it's just wait 10s. 

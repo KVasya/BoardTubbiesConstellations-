14500deeeb7dc1ffca2ae2833551a51dfc3d8403
rashid <rash.ik@yandex.ru>
2016-07-04 03:32:44 +0300

    file non-availability bug removal

--- client.py
+++ client.py
@@ -21,14 +21,18 @@ url = "http://127.0.0.1:5000/upload"
     # sleep(10)
 ''' Testing part ENDs'''
 
-img_to_sent = 'D:/BOARD_PROJECT/test_images/1.bmp' # write your own path + filename
+img_to_sent = 'D:/BOARD_PROJECT/test_images/12.jpg' # write your own path + filename
 
 while True:
-    # opens img_to_sent file, constructs a dict from data (img_to_sent)
-    file = {'file': open(img_to_sent, 'rb')}
+    # opens img_to_sent file
+    with open(img_to_sent, 'rb') as f :
+        # constructs a dict from data (img_to_sent)
+        print '--- in with!! ---'
+        file = {'file': f}
+        # sends the image to the server via post request
+        r = requests.post(url, files=file)
+    print "--- out of with! ---"
     
-    # sends the image to the server via post request
-    r = requests.post(url, files=file)
     
     # waits 10 sec. before sending again.
     sleep(10) # One SHOULD have a condition when it's to send POST request but now it's just wait 10s. 
